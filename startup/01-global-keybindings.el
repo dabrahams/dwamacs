4dc40520fe4b635984c49b20c197ae11abc0e836
Dave Abrahams <dave@boostpro.com>
2011-02-24 14:28:49 -0500

    Use the new me-minor-mode trick

--- startup/01-global-keybindings.el
+++ startup/01-global-keybindings.el
@@ -1,19 +1,9 @@
-;; Copyright David Abrahams 2010. Distributed under the Boost
-;; Software License, Version 1.0. (See accompanying
-;; file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
-;; Contains all the very basic stuff I need in order to not tear my
-;; hair out when using Emacs; mostly keybindings.  This file gets
-;; loaded very early in case something goes wrong during startup and I
-;; end up in the debugger: the environment should already be
-;; relatively good.
-
 ;; Navigation by words
-(global-set-key [(control ?,)] 'backward-word)
-(global-set-key [(control ?.)] 'forward-word)
+(define-key me-minor-mode-map [(control ?,)] 'backward-word)
+(define-key me-minor-mode-map [(control ?.)] 'forward-word)
 
 ;; Navigation to other windows (panes)
-(global-set-key "\C-x\C-n" 'other-window)  ; Normally bound to set-goal-column
+(define-key me-minor-mode-map "\C-x\C-n" 'other-window)  ; Normally bound to set-goal-column
 
 (defun dwa/other-window-backward (&optional n)
   "Select the previous window. Copied from \"Writing Gnu Emacs Extensions\"."
@@ -21,11 +11,11 @@
   (other-window (- (or n 1)))
   )
 
-(global-set-key "\C-x\C-p" 'dwa/other-window-backward) ; Normally bound to mark-page
+(define-key me-minor-mode-map "\C-x\C-p" 'dwa/other-window-backward) ; Normally bound to mark-page
 
 ;; This is the way I like it; the defaults go to the beginning and/or end of line
-(global-set-key [home] 'beginning-of-buffer)
-(global-set-key [end] 'end-of-buffer)
+(define-key me-minor-mode-map [home] 'beginning-of-buffer)
+(define-key me-minor-mode-map [end] 'end-of-buffer)
 
 (defun dwa/match-paren (arg)
   (interactive "P")
@@ -41,7 +31,7 @@
            ;; (insert "%")  ; insert the character we're bound to
       ))))
 
-(global-set-key [( control ?\( )] 'dwa/match-paren)
+(define-key me-minor-mode-map [( control ?\( )] 'dwa/match-paren)
 
 (defun dwa/other-buffer ()
   "Switch to the most recently visited buffer without asking"
@@ -51,20 +41,15 @@
 ;; This is normally set to bring up a buffer list, but there are many other
 ;; ways to do this seldom-desired function (e.g. C-mouse1, or look at the
 ;; "Buffers" menu at the top of the frame).
-(global-set-key "\C-x\C-b" 'dwa/other-buffer)
+(define-key me-minor-mode-map "\C-x\C-b" 'dwa/other-buffer)
 
 ;; Miscellaneous
-(global-set-key "\C-x\C-g" 'goto-line)
+(define-key me-minor-mode-map "\C-x\C-g" 'goto-line)
 
 (defun dwa/kill-current-buffer ()
   "Kill the current buffer without asking, unless it's modified file, in which case ask first"
   (interactive)
   (kill-buffer (current-buffer)))
 
-(global-set-key "\C-x\C-k" 'dwa/kill-current-buffer)
-
-(if (eq system-type 'darwin)
-    ;; This is not important enough to abort startup on failure
-    (ignore-errors
-      (require 'osx-plist)
-      (osx-plist-update-environment)))
+(define-key me-minor-mode-map "\C-x\C-k" 'dwa/kill-current-buffer)
+(define-key me-minor-mode-map "\C-xg" 'magit-status)
