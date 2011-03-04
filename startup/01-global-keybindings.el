0edae607607cb71cd9ce44cc94bfb07a1e632203
Dave Abrahams <dave@boostpro.com>
2011-03-04 13:08:39 -0500

    Add back a bunch of global functionality, e.g. one-key "compile"

--- startup/01-global-keybindings.el
+++ startup/01-global-keybindings.el
@@ -53,3 +53,60 @@
 
 (define-key me-minor-mode-map "\C-x\C-k" 'dwa/kill-current-buffer)
 (define-key me-minor-mode-map "\C-xg" 'magit-status)
+
+;;
+;; my-compile, my-recompile - easy compilation with scrolling errors, and easy
+;;      recompilation without worrying about what buffer you're in.
+;;
+
+;; Used by my-compile and my-recompile to get back to the bottom of a
+;; compilation buffer after save-excursion brings us back to the place we
+;; started.
+(defun my-end-of-current-compilation-buffer()
+  (if (equal (buffer-name) "*compilation*")
+      (end-of-buffer)))
+  
+(defun my-compile(&optional command)
+  (interactive)
+  (if (interactive-p)
+      (call-interactively 'compile)
+    (compile command))
+  (save-excursion
+    (pop-to-buffer "*compilation*")
+    (end-of-buffer))
+  ;; force scrolling despite save-excursion
+  (my-end-of-current-compilation-buffer))
+
+(defun my-buffer-exists (buffer)
+  "Return t if the buffer exists.
+buffer is either a buffer object or a buffer name"
+  (bufferp (get-buffer buffer)))
+  
+(defun my-recompile ()
+  "Run recompilation but put the point at the *end* of the buffer
+so we can watch errors as they come up"
+  (interactive)
+  (if (and (my-buffer-exists "*compilation*")
+           compile-command)
+      (save-excursion
+        ;; switching to the compilation buffer here causes the compile command to be
+        ;; executed from the same directory it originated from.
+        (pop-to-buffer "*compilation*")
+        (recompile)
+        (pop-to-buffer "*compilation*")
+        (end-of-buffer))
+    ;; else
+    (call-interactively 'my-compile))
+  ;; force scrolling despite save-excursion
+  (my-end-of-current-compilation-buffer))
+
+(add-hook 'compilation-mode-hook (lambda () (visual-line-mode t)))
+
+(define-key me-minor-mode-map [f7] 'my-recompile)
+(define-key me-minor-mode-map [(control f7)] 'my-compile)
+(define-key me-minor-mode-map [f4] 'next-error)
+(define-key me-minor-mode-map [(shift f4)] 'previous-error)
+(define-key me-minor-mode-map [(control f4)] 'first-error)
+
+(autoload 'my-wl-check-mail-primary "wl")
+(define-key me-minor-mode-map "\C-xM" 'my-wl-check-mail-primary)
