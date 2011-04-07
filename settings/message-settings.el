1c35555f8364dc6823625c7f97b7daea6393a64f
Dave Abrahams <dave@boostpro.com>
2011-04-07 16:07:27 -0400

    Separated message settings and reconstituted a missing function

---
+++ settings/message-settings.el
@@ -0,0 +1,37 @@
+(defun message-insert-spam-resistant-citation-line ()
+  "Insert a simple citation line that slightly obscures the email address of the sender."
+
+  (when message-reply-headers
+    (insert "\non " 
+            (format-time-string 
+             "%a %b %d %Y" (date-to-time (mail-header-date message-reply-headers))) 
+            ", "
+            (replace-regexp-in-string "@" "-AT-" (mail-header-from message-reply-headers))
+            " wrote:\n\n")) )
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(message-citation-line-function (quote message-insert-spam-resistant-citation-line))
+ '(message-cite-prefix-regexp "\\([ 	]*[_.[:word:]]+>+\\|[ 	]*[]>|]\\)+" nil nil "
+Removed \"}\" from the allowable characters because I often type that when writing replies.")
+ '(message-default-headers "Bcc: dave@boostpro.com
+" nil nil "
+Always Bcc: myself")
+ '(message-forward-ignored-headers (quote ("^Content-Transfer-Encoding:" "^X-Gnus" "^X-" "^Received:" "^User-Agent:" "^References:")))
+ '(message-mode-hook (quote ((lambda nil (auto-fill-mode t)))) nil nil "
+Automatically wrap text during email composition")
+ '(message-send-mail-function (quote message-send-mail-with-sendmail))
+ '(message-subject-re-regexp "^[ 	]*\\(\\([Rr][Ee]\\|[Aa][Ww]\\)\\(\\[[0-9]*\\]\\)*:[ 	]*\\)*[ 	]*" nil nil "
+Handle Germans' Aw: version of Re:")
+ '(message-subject-trailing-was-query t nil nil "
+always strip the trailing old subject in (was: ...) subjects")
+ '(message-syntax-checks (quote ((sender . disabled) (long-lines . disabled))) nil nil "
+Don't complain about long lines, please"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
--- settings/settings.el
+++ settings/settings.el
@@ -80,23 +80,6 @@ from macports.")
  '(mairix-mail-program (quote wl))
  '(mairix-search-file ".zz_mairix-results")
  '(markdown-command "markdown-extra")
- '(message-citation-line-function (quote my-message-insert-citation-line) nil nil "
-Make sure peoples' email addresses are (trivially) obscured")
- '(message-cite-prefix-regexp "\\([ 	]*[_.[:word:]]+>+\\|[ 	]*[]>|]\\)+" nil nil "
-Removed \"}\" from the allowable characters because I often type that when writing replies.")
- '(message-default-headers "Bcc: dave@boostpro.com
-" nil nil "
-Always Bcc: myself")
- '(message-forward-ignored-headers (quote ("^Content-Transfer-Encoding:" "^X-Gnus" "^X-" "^Received:" "^User-Agent:" "^References:")))
- '(message-mode-hook (quote ((lambda nil (auto-fill-mode t)))) nil nil "
-Automatically wrap text during email composition")
- '(message-send-mail-function (quote message-send-mail-with-sendmail))
- '(message-subject-re-regexp "^[ 	]*\\(\\([Rr][Ee]\\|[Aa][Ww]\\)\\(\\[[0-9]*\\]\\)*:[ 	]*\\)*[ 	]*" nil nil "
-Handle Germans' Aw: version of Re:")
- '(message-subject-trailing-was-query t nil nil "
-always strip the trailing old subject in (was: ...) subjects")
- '(message-syntax-checks (quote ((sender . disabled) (long-lines . disabled))) nil nil "
-Don't complain about long lines, please")
  '(muse-project-alist (quote (("WikiPlanner" ("~/plans" :default "index" :major-mode planner-mode :visit-link planner-visit-link)))))
  '(ns-alternate-modifier (quote control) nil nil "
 I'm continually pressing option when I mean control.  So, I get no
