85aabcfc4d897896b11f4f4a3ee4c810da411569
Dave Abrahams <dave@boostpro.com>
2011-06-12 17:14:51 -0400

    Allow more initsplit pretty-printing and cleanup to take effect, for better Git diffing in future

--- settings/wl-settings.el
+++ settings/wl-settings.el
@@ -423,14 +423,16 @@ when we need it."
 
 ;; Settings
 (custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(elmo-imap4-default-authenticate-type (quote clear))
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(elmo-imap4-default-authenticate-type
+   (quote clear))
  '(elmo-imap4-default-port 993)
  '(elmo-imap4-default-server "imap.gmail.com")
- '(elmo-imap4-default-stream-type (quote ssl))
+ '(elmo-imap4-default-stream-type
+   (quote ssl))
  '(elmo-imap4-default-user "dave@boostpro.com")
  '(elmo-lang "en")
  '(elmo-localdir-folder-path "~/Maildir")
@@ -440,32 +442,56 @@ The default limit is so low that it always asks about messages that would fetch 
  '(elmo-network-session-idle-timeout 300 nil nil "James Harkins' alternative to the nasty workaround in commit b1692de9 of dwamacs")
  '(elmo-nntp-default-server "news.gmane.org")
  '(elmo-search-namazu-default-index-path "~/Maildir")
- '(elmo-spam-scheme (quote bogofilter))
+ '(elmo-spam-scheme
+   (quote bogofilter))
  '(mime-edit-split-message nil nil nil "
 This should really be the default.  Most MUAs can't decode the split messages!")
  '(mime-play-delete-file-immediately nil)
  '(mime-save-directory "/tmp")
- '(mm-attachment-override-types (quote ("text/x-vcard" "application/pkcs7-mime" "application/x-pkcs7-mime" "application/pkcs7-signature" "application/x-pkcs7-signature" "image/*")) nil nil "
+ '(mm-attachment-override-types
+   (quote
+    ("text/x-vcard" "application/pkcs7-mime" "application/x-pkcs7-mime" "application/pkcs7-signature" "application/x-pkcs7-signature" "image/*"))
+   nil nil "
 Added image/* to display attached images inline")
- '(mm-discouraged-alternatives (quote ("text/html" "text/richtext" "image/.*")) nil nil "
+ '(mm-discouraged-alternatives
+   (quote
+    ("text/html" "text/richtext" "image/.*"))
+   nil nil "
 The documentation for this variable says it all")
  '(mm-inline-text-html-with-images t)
  '(wl-ask-range nil nil nil "
 The range thing slows me down.  However, I'd still like to know how to force the question.")
- '(wl-auto-check-folder-list (quote ("Inbox")))
+ '(wl-auto-check-folder-list
+   (quote
+    ("Inbox")))
  '(wl-auto-check-folder-name "Inbox")
- '(wl-auto-select-next (quote skip-no-unread))
- '(wl-auto-uncheck-folder-list (quote ("\\$.*" "%zz_mairix.*" "%zz_archive.*" "%.*")))
- '(wl-dispose-folder-alist (quote (("^-" . remove) ("^@" . remove) ("\\<All Mail\\>" . trash) ("^%" . remove))) nil nil "
+ '(wl-auto-select-next
+   (quote skip-no-unread))
+ '(wl-auto-uncheck-folder-list
+   (quote
+    ("\\$.*" "%zz_mairix.*" "%zz_archive.*" "%.*")))
+ '(wl-dispose-folder-alist
+   (quote
+    (("^-" . remove)
+     ("^@" . remove)
+     ("\\<All Mail\\>" . trash)
+     ("^%" . remove)))
+   nil nil "
 Only disposing of something in \"All Mail\" should actually move it to
 the trash (GMail will clean that up after 30 days).  Otherwise, we can
 fully delete it from the current folder because we always have a copy
 in \"All Mail.\"
 ")
  '(wl-draft-always-delete-myself t)
- '(wl-draft-config-alist (quote (("" (part-bottom . "
-") ("Bcc" . "Dave Abrahams <dave@boostpro.com>") (signature . "~/.signature")))))
- '(wl-draft-reply-buffer-style (quote full))
+ '(wl-draft-config-alist
+   (quote
+    ((""
+      (part-bottom . "
+")
+      ("Bcc" . "Dave Abrahams <dave@boostpro.com>")
+      (signature . "~/.signature")))))
+ '(wl-draft-reply-buffer-style
+   (quote full))
  '(wl-draft-sendlog-max-size 100000 nil nil "
 Keep more sent messages around for quick/easy access
 ")
@@ -473,23 +499,94 @@ Keep more sent messages around for quick/easy access
 This has to be on if I want the 'sendlog folder to contain anything
 ")
  '(wl-fldmgr-add-complete-with-current-folder-list t)
- '(wl-folder-desktop-name #("Messages" 0 8 (wl-folder-entity-id 0 wl-folder-is-group is-group)))
+ '(wl-folder-desktop-name
+   #("Messages" 0 8
+     (wl-folder-entity-id 0 wl-folder-is-group is-group)))
  '(wl-folder-notify-deleted t)
- '(wl-folder-petname-alist (quote (("%INBOX" . "Inbox") ("+drafts" . "Drafts") (#("%[Gmail]/Sent" 0 13 (wl-folder-is-group nil wl-folder-entity-id 3)) . "Sent") (#("%inbox:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 58 (wl-folder-is-group nil wl-folder-entity-id 72)) . "Inbox") (#("%[Gmail]/Trash:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 66 (wl-folder-is-group nil wl-folder-entity-id 74)) . "Trash") (#("%[Gmail]/Star:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 65 (wl-folder-is-group nil wl-folder-entity-id 75)) . "With a Star") (#("%[Gmail]/Sent:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 65 (wl-folder-is-group nil wl-folder-entity-id 76)) . "Sent") (#("%[Gmail]/Draft:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 66 (wl-folder-is-group nil wl-folder-entity-id 77)) . "Draft") (#("%[Gmail]/All E-Mails:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 72 (wl-folder-is-group nil wl-folder-entity-id 78)) . "All E-Mails") (#("%Org-Mode:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 61 (wl-folder-is-group nil wl-folder-entity-id 79)) . "Org-Mode") (#("%[Gmail]/Draft" 0 14 (wl-folder-is-group nil wl-folder-entity-id 4)) . "Drafts") (#("%[Gmail]/Star" 0 13 (wl-folder-is-group nil wl-folder-entity-id 2)) . "Flagged") ("%Trash" . "Trash") (#("%INBOX" 0 6 (wl-folder-entity-id 1 wl-folder-is-group nil)) . "Inbox") (#("%[Gmail]/Starred" 0 16 (wl-folder-entity-id 2 wl-folder-is-group nil)) . "Important") (#("%[Gmail]/Sent Mail" 0 18 (wl-folder-entity-id 3 wl-folder-is-group nil)) . "Sent") (#("%[Gmail]/Drafts" 0 15 (wl-folder-entity-id 4 wl-folder-is-group nil)) . "Drafts") (#("%[Gmail]/All Mail" 0 17 (wl-folder-entity-id 6 wl-folder-is-group nil)) . "Archive") (#("%[Gmail]/Trash" 0 14 (wl-folder-entity-id 7 wl-folder-is-group nil)) . "Trash") (#("%[Gmail]/Spam" 0 13 (wl-folder-entity-id 9 wl-folder-is-group nil)) . "Spam") (#("+draft" 0 6 (wl-folder-entity-id 10 wl-folder-is-group nil)) . "Drafts"))))
- '(wl-folder-process-duplicates-alist (quote (("^.*" . hide))) nil nil "
+ '(wl-folder-petname-alist
+   (quote
+    (("%INBOX" . "Inbox")
+     ("+drafts" . "Drafts")
+     (#("%[Gmail]/Sent" 0 13
+        (wl-folder-is-group nil wl-folder-entity-id 3))
+      . "Sent")
+     (#("%inbox:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 58
+        (wl-folder-is-group nil wl-folder-entity-id 72))
+      . "Inbox")
+     (#("%[Gmail]/Trash:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 66
+        (wl-folder-is-group nil wl-folder-entity-id 74))
+      . "Trash")
+     (#("%[Gmail]/Star:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 65
+        (wl-folder-is-group nil wl-folder-entity-id 75))
+      . "With a Star")
+     (#("%[Gmail]/Sent:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 65
+        (wl-folder-is-group nil wl-folder-entity-id 76))
+      . "Sent")
+     (#("%[Gmail]/Draft:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 66
+        (wl-folder-is-group nil wl-folder-entity-id 77))
+      . "Draft")
+     (#("%[Gmail]/All E-Mails:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 72
+        (wl-folder-is-group nil wl-folder-entity-id 78))
+      . "All E-Mails")
+     (#("%Org-Mode:\"dave.abrahams@gmail.com\"/clear@imap.gmail.com:993!" 0 61
+        (wl-folder-is-group nil wl-folder-entity-id 79))
+      . "Org-Mode")
+     (#("%[Gmail]/Draft" 0 14
+        (wl-folder-is-group nil wl-folder-entity-id 4))
+      . "Drafts")
+     (#("%[Gmail]/Star" 0 13
+        (wl-folder-is-group nil wl-folder-entity-id 2))
+      . "Flagged")
+     ("%Trash" . "Trash")
+     (#("%INBOX" 0 6
+        (wl-folder-entity-id 1 wl-folder-is-group nil))
+      . "Inbox")
+     (#("%[Gmail]/Starred" 0 16
+        (wl-folder-entity-id 2 wl-folder-is-group nil))
+      . "Important")
+     (#("%[Gmail]/Sent Mail" 0 18
+        (wl-folder-entity-id 3 wl-folder-is-group nil))
+      . "Sent")
+     (#("%[Gmail]/Drafts" 0 15
+        (wl-folder-entity-id 4 wl-folder-is-group nil))
+      . "Drafts")
+     (#("%[Gmail]/All Mail" 0 17
+        (wl-folder-entity-id 6 wl-folder-is-group nil))
+      . "Archive")
+     (#("%[Gmail]/Trash" 0 14
+        (wl-folder-entity-id 7 wl-folder-is-group nil))
+      . "Trash")
+     (#("%[Gmail]/Spam" 0 13
+        (wl-folder-entity-id 9 wl-folder-is-group nil))
+      . "Spam")
+     (#("+draft" 0 6
+        (wl-folder-entity-id 10 wl-folder-is-group nil))
+      . "Drafts"))))
+ '(wl-folder-process-duplicates-alist
+   (quote
+    (("^.*" . hide)))
+   nil nil "
 Don't show me any duplicate messages")
  '(wl-folder-window-width 60)
  '(wl-highlight-folder-by-numbers 1)
- '(wl-highlight-x-face-function (quote wl-gravatar-insert))
+ '(wl-highlight-x-face-function
+   (quote wl-gravatar-insert))
  '(wl-icon-directory "~/.emacs.d/el-get/wanderlust/icons/")
  '(wl-interactive-exit nil)
  '(wl-interactive-save-folders nil)
  '(wl-interactive-send nil)
- '(wl-message-ignored-field-list (quote ("^.*:")))
- '(wl-message-sort-field-list (quote ("^From" "^Organization:" "^X-Attribution:" "^Subject" "^Date" "^To" "^Cc")))
- '(wl-message-visible-field-list (quote ("^\\(To\\|Cc\\):" "^Subject:" "^\\(From\\|Reply-To\\):" "^Organization:" "^Message-Id:" "^\\(Posted\\|Date\\):" "^\\(Mailer\\|User-Agent\\):" "^\\(List-Post\\):" "^\\(Xref\\):")))
+ '(wl-message-ignored-field-list
+   (quote
+    ("^.*:")))
+ '(wl-message-sort-field-list
+   (quote
+    ("^From" "^Organization:" "^X-Attribution:" "^Subject" "^Date" "^To" "^Cc")))
+ '(wl-message-visible-field-list
+   (quote
+    ("^\\(To\\|Cc\\):" "^Subject:" "^\\(From\\|Reply-To\\):" "^Organization:" "^Message-Id:" "^\\(Posted\\|Date\\):" "^\\(Mailer\\|User-Agent\\):" "^\\(List-Post\\):" "^\\(Xref\\):")))
  '(wl-smtp-authenticate-type "plain")
- '(wl-smtp-connection-type (quote starttls))
+ '(wl-smtp-connection-type
+   (quote starttls))
  '(wl-smtp-posting-server "smtp.gmail.com")
  '(wl-smtp-posting-user "dave@boostpro.com")
  '(wl-spam-folder "%[Gmail]/Spam")
@@ -505,13 +602,12 @@ wl-dispose-folder-alist is set up so the only messages sent to Trash
 have been marked disposed in an \"All Mail\" folder.  Any others should
 be deleted immediately since there is a copy in All Mail.
 ")
- '(wl-use-folder-petname (quote (modeline ask-folder read-folder)) nil nil "
+ '(wl-use-folder-petname
+   (quote
+    (modeline ask-folder read-folder))
+   nil nil "
 I have some hard-to-type folder names; why struggle?
 ")
- '(wl-user-mail-address-list (quote ("dave@boostpro.com" "dave.abrahams@gmail.com" "daveabrahams@gmail.com" "boost.consulting@gmail.com" "david.abrahams@rcn.com" "dave@luannocracy.com"))))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- )
+ '(wl-user-mail-address-list
+   (quote
+    ("dave@boostpro.com" "dave.abrahams@gmail.com" "daveabrahams@gmail.com" "boost.consulting@gmail.com" "david.abrahams@rcn.com" "dave@luannocracy.com"))))
