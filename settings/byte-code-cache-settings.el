c85376cde98a44573d309f03c0ac64635313eb27
Dave Abrahams <dave@boostpro.com>
2011-08-20 09:10:34 -0500

    Throw out any outdated .elc files before attempting to save a .el

---
+++ settings/byte-code-cache-settings.el
@@ -0,0 +1,11 @@
+(defun dwa/kill-corresponding-elc-file ()
+  "When the current buffer is an elisp source file, delete
+any corresponding compiled .elc file"
+  (when (and (buffer-file-name) 
+             (string-match "\\`\\(.*\\)[.]el\\(?:[.]gz\\)?\\'" (buffer-file-name)))
+    (let ((elc (concat (match-string 1 (buffer-file-name)) ".elc")))
+      (when (file-exists-p elc)
+        (delete-file elc)))))
+
+;; Be sure not to leave around any outdated .elc files
+(add-hook 'before-save-hook 'dwa/kill-corresponding-elc-file)
