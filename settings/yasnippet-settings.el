74cb0a19e15ed2e5ca709e32a349cb4cc231b7a8
Dave Abrahams <dave@boostpro.com>
2011-08-22 18:59:45 -0500

    Add my snippets directory to the default set

---
+++ settings/yasnippet-settings.el
@@ -0,0 +1,8 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(yas/snippet-dirs
+   (quote
+    ("~/.emacs.d/el-get/dwamacs/snippets" "~/.emacs.d/el-get/yasnippet/snippets"))))
