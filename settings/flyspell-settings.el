08c4f6f771f6ee121dda96e14236532e347dbf81
Dave Abrahams <dave@boostpro.com>
2011-08-22 16:40:40 -0500

    Don't let flyspell overwrite my next/prev-word bindings

---
+++ settings/flyspell-settings.el
@@ -0,0 +1,5 @@
+(let ((map flyspell-mode-map))
+    (define-key map [(control ?\,)] 'backward-word)
+    (define-key map [(control ?\.)] 'forward-word)
+    (define-key map [(control meta ?\,)] 'flyspell-goto-next-error)
+    (define-key map [(control meta ?\.)] 'flyspell-auto-correct-word))
