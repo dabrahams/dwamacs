8734635f5c2ba256989b8ec3b9f927128234391c
Dave Abrahams <dave@boostpro.com>
2011-08-22 17:31:27 -0500

    Add buildbot and python docs to my Info path

--- settings/settings.el
+++ settings/settings.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(Info-additional-directory-list
+   (quote
+    ("~/src/buildbot/master/docs/" "~/Library/Info/python/")))
  '(ac-clang-flags
    (quote
     ("-I" "/Users/dave/src/boost-svn-git" "-I" "/opt/local/Library/Frameworks/Python.framework/Versions/2.7/include/python2.7/")))
