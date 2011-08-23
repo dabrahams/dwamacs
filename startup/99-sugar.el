a9ee143310781e3c07bce544ffb460986a7f37f8
Dave Abrahams <dave@boostpro.com>
2011-08-22 19:02:30 -0500

    Various niceties

--- startup/99-sugar.el
+++ startup/99-sugar.el
@@ -15,6 +15,42 @@
 (remove-hook 'gnus-summary-mode-hook 'ffap-gnus-hook)
 (remove-hook 'gnus-article-mode-hook 'ffap-gnus-hook)
 
+;;;;;;;;
+
+(require 'session)
+(add-hook 'after-init-hook 'session-initialize)
+
+;;;;;;;
+
+(ignore-errors
+  (zenburn))
+
+(setq frame-title-format
+    '(:eval
+      (concat "%["
+              (if buffer-file-name default-directory "%b")
+              " - %I%]")))
+
+;; Make sure any customizations are saved before exiting.  Should
+;; eventually be replaced by the use of cus-edit+
+(defun dwa/save-customizations-before-exit ()
+  (condition-case err 
+      (progn (customize-unsaved) nil)
+    (error 
+     (or (equal err '(error "No user options are set but unsaved"))
+         (apply 'signal err)))))
+(add-to-list 'kill-emacs-query-functions 'dwa/save-customizations-before-exit)
+
+(ignore-errors (require 'elscreen))
+
+;; The git pager sux except in a terminal
+(setenv "GIT_PAGER" "")
+
+;; Makes `C-c RET C-a' send the current file as an attachment in dired
+;; [[message://m2vcukdcsu.fsf@gmail.com]]
+(autoload 'gnus-dired-mode "gnus-dired" nil t)
+(add-hook 'dired-mode-hook 'gnus-dired-mode)
+
 ;; Maximize emacs on startup
 (ignore-errors
   (require 'maxframe)
