60c865edd516456d8f8b74d2d49b8866e62a68c9
Dave Abrahams <dave@boostpro.com>
2011-09-04 09:27:10 -0800

    Add support for marmalade repo

--- startup/30-setups.el
+++ startup/30-setups.el
@@ -3,3 +3,8 @@
 
 (autoload 'svn-status "dsvn" "Run `svn status'." t)
 (autoload 'svn-update "dsvn" "Run `svn update'." t)
+
+(eval-after-load 'package
+  (add-to-list
+   'package-archives
+   '("marmalade" . "http://marmalade-repo.org/packages/")))
