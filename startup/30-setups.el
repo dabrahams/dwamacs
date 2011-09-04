c7f0b3709e00573f85081b6f1a7004dc04b8e3dd
Dave Abrahams <dave@boostpro.com>
2011-09-04 09:23:58 -0800

    org-magit now handles its own autoloads

--- startup/30-setups.el
+++ startup/30-setups.el
@@ -1,7 +1,2 @@
 (eval-after-load 'info
   (require 'info+ nil 'noerror))
-
-(autoload 'org-magit-store-link "org-magit")
-(autoload 'org-magit-open "org-magit")
-(autoload 'org-magit-export "org-magit")
-(add-hook 'org-store-link-functions 'org-magit-store-link)
