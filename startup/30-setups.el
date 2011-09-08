1da4e0e65ff5ab68e0f35ea3e6a038931d705782
Dave Abrahams <dave@boostpro.com>
2011-09-08 19:36:11 -0400

    BUGFIX: quote the eval-after-loads

--- startup/30-setups.el
+++ startup/30-setups.el
@@ -1,10 +1,10 @@
 (eval-after-load 'info
-  (require 'info+ nil 'noerror))
+  '(require 'info+ nil 'noerror))
 
 (autoload 'svn-status "dsvn" "Run `svn status'." t)
 (autoload 'svn-update "dsvn" "Run `svn update'." t)
 
 (eval-after-load 'package
-  (add-to-list
+  '(add-to-list
    'package-archives
    '("marmalade" . "http://marmalade-repo.org/packages/")))
