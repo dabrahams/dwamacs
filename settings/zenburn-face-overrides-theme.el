04a9f34124422ecaf1fdb5152c442ab181d15841
Dave Abrahams <dave@boostpro.com>
2011-09-15 21:28:17 -0400

    Add a theme to accomodate my zenburn customizations

---
+++ settings/zenburn-face-overrides-theme.el
@@ -0,0 +1,8 @@
+(deftheme zenburn-face-overrides
+  "Created 2011-09-14.")
+
+(custom-theme-set-faces
+ 'zenburn-face-overrides
+ '(org-level-4 ((t (:inherit (zenburn-green+3))))))
+
+(provide-theme 'zenburn-face-overrides)
