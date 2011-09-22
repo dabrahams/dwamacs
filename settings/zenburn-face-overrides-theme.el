a7fbe8d71a8862a389b515327a2d956961c8ffc0
Dave Abrahams <dave@boostpro.com>
2011-09-22 19:37:36 -0400

    WIP: Theme-ify my own zenburn customizations

--- settings/zenburn-face-overrides-theme.el
+++
@@ -1,8 +0,0 @@
-(deftheme zenburn-face-overrides
-  "Created 2011-09-14.")
-
-(custom-theme-set-faces
- 'zenburn-face-overrides
- '(org-level-4 ((t (:inherit (zenburn-green+3))))))
-
-(provide-theme 'zenburn-face-overrides)
