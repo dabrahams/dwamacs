66cddf8f93882bd121a250714c250a8cda8d4a87
Dave Abrahams <dave@boostpro.com>
2011-09-22 19:27:41 -0400

    Unwind nasty autoload dependency

--- settings/org-settings.el
+++ settings/org-settings.el
@@ -1,24 +1,5 @@
 ;;; -*- mode: emacs-lisp -*-
 
-;;;###autoload
-(autoload 'jump-to-org-agenda "org" "" t)
-(defun jump-to-org-agenda ()
-  (interactive)
-  (let ((buf (get-buffer "*Org Agenda*"))
-        wind)
-    (if buf
-        (if (setq wind (get-buffer-window buf))
-            (when (called-interactively-p 'any)
-              (select-window wind)
-              (org-fit-window-to-buffer))
-          (if (called-interactively-p 'any)
-              (progn
-                (select-window (display-buffer buf t t))
-                (org-fit-window-to-buffer))
-            (with-selected-window (display-buffer buf)
-              (org-fit-window-to-buffer))))
-      (call-interactively 'org-agenda-list))))
-
 ;;;_* customizations
 
 ;;;_ + variables
---
+++ site-lisp/org-jump.el
@@ -0,0 +1,17 @@
+;;;###autoload
+(defun jump-to-org-agenda ()
+  (interactive)
+  (let ((buf (get-buffer "*Org Agenda*"))
+        wind)
+    (if buf
+        (if (setq wind (get-buffer-window buf))
+            (when (called-interactively-p 'any)
+              (select-window wind)
+              (org-fit-window-to-buffer))
+          (if (called-interactively-p 'any)
+              (progn
+                (select-window (display-buffer buf t t))
+                (org-fit-window-to-buffer))
+            (with-selected-window (display-buffer buf)
+              (org-fit-window-to-buffer))))
+      (call-interactively 'org-agenda-list))))
