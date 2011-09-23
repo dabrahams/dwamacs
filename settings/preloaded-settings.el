ffefee12734235ec90da76c7dbf8b5661b45ce76
Dave Abrahams <dave@boostpro.com>
2011-09-22 23:18:03 -0400

    For a little extra insurance; preload the setting for gnus-directory

--- settings/preloaded-settings.el
+++ settings/preloaded-settings.el
@@ -3,5 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(gnus-directory "~/Library/Data/Gnus/News/")
  '(gnus-home-directory "~/Library/Data/Gnus/")
  '(message-directory "~/Library/Data/Gnus/Mail/"))
