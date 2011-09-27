9569ae4263b395ef862c34d1c896a9710343b55b
Dave Abrahams <dave@boostpro.com>
2011-09-26 21:21:47 -0400

    Try a bright green cursor for a while

--- settings/settings.el
+++ settings/settings.el
@@ -502,10 +502,6 @@ where I don't expect it to be keeping undo history anyway")
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor
-   ((default
-      (:background "brown"))
-    (nil nil)))
  '(diff-refine-change
    ((((background light))
      (:background "#FFFFC0"))))
--- settings/zenburn-overrides-theme.el
+++ settings/zenburn-overrides-theme.el
@@ -3,6 +3,7 @@
 
 (custom-theme-set-faces
  'zenburn-overrides
+ '(cursor ((t (:foreground "#3f3f3f" :background "#7fff00"))))
  '(mode-line ((((background dark)) (:box (:line-width 2 :color "#1e2320" :style released-button) :foreground "#acbc90" :background "#1e2320"))))
  '(mode-line-inactive ((((background dark)) (:box (:line-width 2 :color "#2f3130" :style nil) :foreground "#767e68" :background "#2f3130"))))
  '(org-level-4 ((t (:inherit (zenburn-green+3))))))
