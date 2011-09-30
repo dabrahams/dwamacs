56abda5d7440ec46c20b9a705cb873330b02a80f
Dave Abrahams <dave@boostpro.com>
2011-09-30 11:39:52 -0400

    Turn on GUD tooltips, for what they're worth (they don't chase pointers)

--- settings/gud-settings.el
+++ settings/gud-settings.el
@@ -58,4 +58,9 @@
 (define-key gud-minor-mode-map [(control f10)] 'my-gud-run-to-cursor)
 (define-key gud-minor-mode-map [(f9)] 'gud-break)
 (define-key gud-minor-mode-map [(shift control f9)] 'gud-remove)
-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(gud-tooltip-mode t))
