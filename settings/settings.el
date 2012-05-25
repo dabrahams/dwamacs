65462510c6a6b36bdd27c52fe9012d8a1962c872
Dave Abrahams <dave@boostpro.com>
2012-05-24 23:15:45 -0400

    We don't need Lucida Mono EF now that we have Ubuntu Mono!

--- settings/settings.el
+++ settings/settings.el
@@ -574,9 +574,6 @@ where I don't expect it to be keeping undo history anyway")
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bold
-   ((t
-     (:weight bold :family "LucidaMonoEF"))))
  '(bold-italic
    ((t
      (:inherit
