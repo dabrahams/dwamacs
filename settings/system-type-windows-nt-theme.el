19333056e3a22c60f99f65c060d489234aae701a
Dave Abrahams <dave@boostpro.com>
2012-08-27 14:37:45 -0700

    More changes to support Windows

--- settings/system-type-windows-nt-theme.el
+++ settings/system-type-windows-nt-theme.el
@@ -1,15 +1,8 @@
-(add-to-list 'exec-path "C:/msysgit/bin")
-(add-to-list 'exec-path "C:/msysgit/cmd")
-
-(setenv "PATH" (mapconcat (lambda (s) (replace-regexp-in-string "/" "\\\\" s)) exec-path ";"))
-
-(let ((existing-font (assoc 'font default-frame-alist)))
-  (if existing-font
-    (setcdr existing-font "Lucida Console-12")
-    (add-to-list 'default-frame-alist '(font . "Lucida Console-12"))))
-  
-
 (deftheme system-type-windows-nt
   "Created 2012-08-27.")
+
+(custom-theme-set-variables
+ 'system-type-windows-nt
+ '(default-frame-alist (quote ((vertical-scroll-bars) (font . "Lucida Console-11")))))
 
 (provide-theme 'system-type-windows-nt)
