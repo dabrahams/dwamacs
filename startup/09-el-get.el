d6fdd566ce7b34828989abe5fdfd93fbe707bff0
Dave Abrahams <dave@boostpro.com>
2012-08-31 18:23:01 -0700

    Make sure personal el-get recipes are available before (el-get) is called

---
+++ startup/09-el-get.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path (expand-file-name (elhome-path-join user-emacs-directory "el-get" "el-get")))
+(require 'el-get)
+(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/dwamacs/el-get-recipes/personal/")
--- startup/11-el-get.el
+++
@@ -1,1 +0,0 @@
-(add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/dwamacs/el-get-recipes/personal/")
