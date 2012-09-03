51288179e73b687c8b7af67aace8bec40e513f4f
Dave Abrahams <dave@boostpro.com>
2012-09-03 12:50:26 -0700

    Stop hiding signature in Gnus
    Too much useful info appears there (especially bug tracker links)

--- settings/gnus-settings.el
+++ settings/gnus-settings.el
@@ -249,7 +249,6 @@ NOTICE: ")))
  '(gnus-treat-from-gravatar
    (quote head))
  '(gnus-treat-hide-citation t)
- '(gnus-treat-hide-signature t)
  '(gnus-treat-mail-gravatar
    (quote head))
  '(gnus-treat-strip-cr t)
