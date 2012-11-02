874b2a8aa5e8138b6a27add2b2e7be08274a2887
Dave Abrahams <dave@boostpro.com>
2012-11-02 09:57:58 -0400

    Use the standard el-get startup.  Disable cedet completely for now

--- init.el
+++ init.el
@@ -2,9 +2,21 @@
 ;; dwamacs initialization ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'find-func)
-(let ((cedet-lisp (file-name-directory (find-library-name "cedet"))))
-  (setq load-path (delete cedet-lisp (delete (directory-file-name cedet-lisp) load-path))))
-(message "load-path: %s" load-path)
+;;(let ((cedet-lisp (file-name-directory (find-library-name "cedet"))))
+;;  (setq load-path (delete cedet-lisp (delete (directory-file-name cedet-lisp) load-path))))
+
+(add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+
+(if (require 'el-get nil t)
+    (el-get 'sync)
+  (url-retrieve
+   "https://raw.github.com/dimitri/el-get/master/el-get-install.el"
+   (lambda (s)
+     (let (el-get-master-branch)
+       (goto-char (point-max))
+       (eval-print-last-sexp)))))
+
+(el-get 'sync)
 
 (defun el-get-subdir
   (&rest d)
