c080859d5580129f0cca662498be4512eb800342
Dave Abrahams <dave@boostpro.com>
2012-11-06 23:03:18 -0500

    Make sure osx-plist stuff has no effect on non-Darwin platforms

--- startup/20-envvars.el
+++ startup/20-envvars.el
@@ -4,19 +4,19 @@
 ;; automatically, but this works, at least.
 
 ;; This is not important enough to abort startup on failure
-(require 'osx-plist nil 'noerror)
-
 (eval-when-compile
   (require 'cl))
 
-;; Replace the implementation of osx-plist-update-exec-path with one
-;; that respects the ordering of $PATH and avoids duplicates
-(defun osx-plist-update-exec-path ()
-  "Update `exec-path' from the PATH environment variable."
-  (let ((path (delq nil (parse-colon-path (getenv "PATH")))))
-    (setq exec-path
-          (dolist (dir exec-path path)
-            (add-to-list 'path (file-name-as-directory dir) :append)))))
+(when (and (eq system-type 'darwin)
+           (require 'osx-plist nil 'noerror))
+  ;; Replace the implementation of osx-plist-update-exec-path with one
+  ;; that respects the ordering of $PATH and avoids duplicates
+  (defun osx-plist-update-exec-path ()
+    "Update `exec-path' from the PATH environment variable."
+    (let ((path (delq nil (parse-colon-path (getenv "PATH")))))
+      (setq exec-path
+            (dolist (dir exec-path path)
+              (add-to-list 'path (file-name-as-directory dir) :append)))))
 
-(when (and (featurep 'osx-plist) (eq system-type 'darwin))
   (osx-plist-update-environment))
+
