1cbe7a39e594d39c97a4b5774e598f24a723b657
Dave Abrahams <dave@boostpro.com>
2012-11-11 00:21:18 -0500

    Add melpa to package repositories

--- settings/settings.el
+++ settings/settings.el
@@ -232,7 +232,8 @@ command.")
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
-     ("marmalade" . "http://marmalade-repo.org/packages/"))))
+     ("marmalade" . "http://marmalade-repo.org/packages/")
+     ("melpa" . "http://melpa.milkbox.net/packages/"))))
  '(pp^L-^L-string "····································✄····································
 ")
  '(ps-font-family
