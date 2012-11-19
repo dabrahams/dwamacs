5246a862bcf9cb5a6d5a8781c80dc533015cbe76
Dave Abrahams <dave@boostpro.com>
2012-11-19 10:49:57 -0500

    Don't save debug-on-xxxx across sessions

--- settings/session-settings.el
+++ settings/session-settings.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(session-globals-exclude
+   (quote
+    ("\\`helm-c-source" "\\`helm-c-source" load-history register-alist vc-comment-ring flyspell-auto-correct-ring org-mark-ring planner-browser-file-display-rule-ring debug-on-error debug-on-quit)))
  '(session-use-package t nil
                        (session)))
 
