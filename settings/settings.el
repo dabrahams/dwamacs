4c1ba1dcab2f6e111a96a1f66957bac1cee78381
Dave Abrahams <dave@boostpro.com>
2013-02-18 11:00:10 -0800

    Grab a bunch of JW's Org config

--- settings/settings.el
+++ settings/settings.el
@@ -231,6 +231,283 @@ command.")
    (quote hyper))
  '(ns-right-command-modifier
    (quote super))
+ '(org-adapt-indentation nil)
+ '(org-agenda-auto-exclude-function
+   (quote org-my-auto-exclude-function))
+ '(org-agenda-custom-commands
+   (quote
+    (("E" "Errands (next 3 days)" tags "Errand&TODO<>\"DONE\"&TODO<>\"CANCELED\"&STYLE<>\"habit\"&SCHEDULED<\"<+3d>\""
+      ((org-agenda-overriding-header "Errands (next 3 days)")))
+     ("A" "Priority #A tasks" agenda ""
+      ((org-agenda-ndays 1)
+       (org-agenda-overriding-header "Today's priority #A tasks: ")
+       (org-agenda-skip-function
+        (quote
+         (org-agenda-skip-entry-if
+          (quote notregexp)
+          "\\=.*\\[#A\\]")))))
+     ("b" "Priority #A and #B tasks" agenda ""
+      ((org-agenda-ndays 1)
+       (org-agenda-overriding-header "Today's priority #A and #B tasks: ")
+       (org-agenda-skip-function
+        (quote
+         (org-agenda-skip-entry-if
+          (quote regexp)
+          "\\=.*\\[#C\\]")))))
+     ("w" "Waiting/delegated tasks" tags "TODO=\"WAITING\"|TODO=\"DELEGATED\""
+      ((org-agenda-overriding-header "Waiting/delegated tasks:")
+       (org-agenda-sorting-strategy
+        (quote
+         (todo-state-up priority-down category-up)))))
+     ("p" "Unprioritized tasks" tags "AREA<>\"Work\"&TODO<>\"\"&TODO<>{DONE\\|CANCELED\\|NOTE\\|PROJECT\\|DEFERRED\\|SOMEDAY}"
+      ((org-agenda-files
+        (quote
+         ("~/Documents/Tasks/todo.txt")))
+       (org-agenda-overriding-header "Unprioritized tasks: ")
+       (org-agenda-skip-function
+        (quote
+         (org-agenda-skip-entry-if
+          (quote regexp)
+          "\\=.*\\[#[A-Z]\\]")))))
+     ("u" "Unscheduled tasks" tags "AREA<>\"Work\"&TODO<>\"\"&TODO<>{DONE\\|CANCELED\\|NOTE\\|PROJECT}"
+      ((org-agenda-files
+        (quote
+         ("~/Documents/Tasks/todo.txt")))
+       (org-agenda-overriding-header "Unscheduled tasks: ")
+       (org-agenda-skip-function
+        (quote
+         (org-agenda-skip-entry-if
+          (quote scheduled)
+          (quote deadline)
+          (quote timestamp)
+          (quote regexp)
+          "\\* \\(DEFERRED\\|SOMEDAY\\)")))
+       (org-agenda-sorting-strategy
+        (quote
+         (priority-down)))))
+     ("U" "Deferred tasks" tags "TODO=\"DEFERRED\""
+      ((org-agenda-files
+        (quote
+         ("~/Documents/Tasks/todo.txt")))
+       (org-agenda-overriding-header "Deferred tasks:")))
+     ("Y" "Someday tasks" tags "TODO=\"SOMEDAY\""
+      ((org-agenda-overriding-header "Someday tasks:")))
+     ("G" "Ledger tasks (all)" alltodo ""
+      ((org-agenda-files
+        (quote
+         ("~/src/ledger/plan/TODO")))
+       (org-agenda-overriding-header "Ledger tasks:")
+       (org-agenda-sorting-strategy
+        (quote
+         (todo-state-up priority-down category-up)))))
+     ("N" "Ledger tasks (all, alphabetical)" alltodo ""
+      ((org-agenda-files
+        (quote
+         ("~/src/ledger/plan/TODO")))
+       (org-agenda-overriding-header "Ledger tasks, alphabetical:")
+       (org-agenda-sorting-strategy
+        (quote
+         (alpha-up)))))
+     ("l" "Ledger tasks" tags-todo "TODO<>{SOMEDAY\\|DEFERRED}"
+      ((org-agenda-files
+        (quote
+         ("~/src/ledger/plan/TODO")))
+       (org-agenda-overriding-header "Ledger tasks:")
+       (org-agenda-sorting-strategy
+        (quote
+         (todo-state-up priority-down category-up)))
+       (org-agenda-skip-function
+        (quote
+         (org-agenda-skip-entry-if
+          (quote regexp)
+          "\\=.*\\[#C\\]")))))
+     ("r" "Uncategorized items" tags "CATEGORY=\"Inbox ===>\"&LEVEL=2&TODO<>{DONE\\|CANCELED\\|NOTE\\|PROJECT}"
+      ((org-agenda-overriding-header "Uncategorized items")))
+     ("V" "Unscheduled work-related tasks" tags "AREA=\"Work\"&TODO<>\"\"&TODO<>{DONE\\|CANCELED\\|NOTE\\|PROJECT}"
+      ((org-agenda-overriding-header "Unscheduled work-related tasks")
+       (org-agenda-files
+        (quote
+         ("~/Documents/Tasks/todo.txt")))
+       (org-agenda-sorting-strategy
+        (quote
+         (category-up)))
+       (org-agenda-skip-function
+        (quote
+         (org-agenda-skip-entry-if
+          (quote scheduled)
+          (quote deadline)
+          (quote timestamp)
+          (quote regexp)
+          "\\* \\(DEFERRED\\|SOMEDAY\\)")))))
+     ("W" "Work-related tasks" tags "AREA=\"Work\"&TODO<>\"\"&TODO<>{DONE\\|CANCELED\\|NOTE\\|PROJECT}"
+      ((org-agenda-overriding-header "Work-related tasks")
+       (org-agenda-files
+        (quote
+         ("~/Documents/Tasks/todo.txt")))
+       (org-agenda-sorting-strategy
+        (quote
+         (category-up priority-down todo-state-up alpha-up)))
+       (org-agenda-skip-function
+        (quote
+         (org-agenda-skip-entry-if
+          (quote regexp)
+          "\\* \\(DEFERRED\\|SOMEDAY\\)"))))))))
+ '(org-agenda-deadline-leaders
+   (quote
+    ("D: " "D%d: ")))
+ '(org-agenda-deadline-relative-text "D%d: ")
+ '(org-agenda-deadline-text "D: ")
+ '(org-agenda-default-appointment-duration 60)
+ '(org-agenda-files
+   (quote
+    ("~/Documents/Tasks/todo.txt")))
+ '(org-agenda-follow-indirect t)
+ '(org-agenda-fontify-priorities t)
+ '(org-agenda-include-diary t)
+ '(org-agenda-ndays 1)
+ '(org-agenda-persistent-filter t)
+ '(org-agenda-prefix-format
+   (quote
+    ((agenda . "  %-11:c%?-12t% s")
+     (timeline . "  % s")
+     (todo . "  %-11:c")
+     (tags . "  %-11:c"))))
+ '(org-agenda-restore-windows-after-quit t)
+ '(org-agenda-scheduled-leaders
+   (quote
+    ("" "S%d: ")))
+ '(org-agenda-scheduled-relative-text "S%d: ")
+ '(org-agenda-scheduled-text "")
+ '(org-agenda-show-all-dates t)
+ '(org-agenda-skip-deadline-if-done t)
+ '(org-agenda-skip-scheduled-if-deadline-is-shown t)
+ '(org-agenda-skip-scheduled-if-done t)
+ '(org-agenda-skip-unavailable-files t)
+ '(org-agenda-sorting-strategy
+   (quote
+    ((agenda habit-down time-up todo-state-up priority-down category-keep)
+     (todo priority-down category-keep)
+     (tags priority-down category-keep)
+     (search category-keep))))
+ '(org-agenda-start-on-weekday nil)
+ '(org-agenda-start-with-follow-mode t)
+ '(org-agenda-tags-column -80)
+ '(org-agenda-text-search-extra-files
+   (quote
+    (agenda-archives)))
+ '(org-agenda-window-setup
+   (quote current-window))
+ '(org-archive-location "TODO-archive::")
+ '(org-archive-save-context-info
+   (quote
+    (time category itags)))
+ '(org-attach-method
+   (quote mv))
+ '(org-babel-load-languages
+   (quote
+    ((emacs-lisp . t)
+     (sh . t))))
+ '(org-capture-templates
+   (quote
+    (("t" "Task" entry
+      (file+headline "~/Documents/Tasks/todo.txt" "Inbox")
+      "* TODO %?
+  SCHEDULED: %t
+  :PROPERTIES:
+  :ID:       %(shell-command-to-string \"uuidgen\")  :CREATED:  %U
+  :END:" :prepend t)
+     ("m" "Message" entry
+      (file+headline "~/Documents/Tasks/todo.txt" "Inbox")
+      "* TODO %?Message %:subject
+  SCHEDULED: %t
+  :PROPERTIES:
+  :MESSAGE:  %a
+  :ID:       %(shell-command-to-string \"uuidgen\")  :CREATED:  %U
+  :END:" :prepend t))))
+ '(org-clock-idle-time 10)
+ '(org-clock-in-resume t)
+ '(org-clock-in-switch-to-state "STARTED")
+ '(org-clock-into-drawer "LOGBOOK")
+ '(org-clock-mode-line-total
+   (quote current))
+ '(org-clock-out-remove-zero-time-clocks t)
+ '(org-clock-out-switch-to-state nil)
+ '(org-clock-persist
+   (quote history))
+ '(org-completion-use-ido t)
+ '(org-confirm-elisp-link-function nil)
+ '(org-confirm-shell-link-function nil)
+ '(org-crypt-disable-auto-save nil)
+ '(org-cycle-global-at-bob t)
+ '(org-deadline-warning-days 14)
+ '(org-default-notes-file "~/Documents/Tasks/todo.txt")
+ '(org-default-priority 67)
+ '(org-directory "~/Documents/Tasks/")
+ '(org-drawers
+   (quote
+    ("PROPERTIES" "CLOCK" "LOGBOOK" "OUT")))
+ '(org-edit-src-content-indentation 0)
+ '(org-enforce-todo-dependencies t)
+ '(org-export-babel-evaluate nil)
+ '(org-extend-today-until 6)
+ '(org-fast-tag-selection-single-key
+   (quote expert))
+ '(org-file-apps
+   (quote
+    (("\\.pptx?\\'" . "open %s")
+     (auto-mode . emacs)
+     ("\\.mm\\'" . default)
+     ("\\.x?html?\\'" . default)
+     ("\\.pdf\\'" . default))))
+ '(org-footnote-section nil)
+ '(org-habit-completed-glyph 10004)
+ '(org-habit-preceding-days 42)
+ '(org-habit-show-habits-only-for-today nil)
+ '(org-habit-today-glyph 9483)
+ '(org-hide-leading-stars t)
+ '(org-id-link-to-org-use-id
+   (quote create-if-interactive))
+ '(org-insert-heading-respect-content t)
+ '(org-irc-link-to-logs t t)
+ '(org-log-into-drawer t)
+ '(org-mobile-directory "~/Dropbox/MobileOrg")
+ '(org-mobile-files nil)
+ '(org-mobile-inbox-for-pull "~/Documents/Tasks/from-mobile.org")
+ '(org-modules
+   (quote
+    (org-id org-info org-habit)))
+ '(org-pretty-entities t)
+ '(org-refile-target-verify-function
+   (quote dwa/org-verify-refile-target))
+ '(org-refile-targets
+   (quote
+    ((nil :todo . "PROJECT")
+     (nil :maxlevel . 2))))
+ '(org-return-follows-link t)
+ '(org-reverse-note-order t)
+ '(org-special-ctrl-a/e
+   (quote reversed))
+ '(org-speed-commands-user
+   (quote
+    (("+" . org-priority-up)
+     ("-" . org-priority-down)
+     (":" . org-set-tags))))
+ '(org-src-fontify-natively t)
+ '(org-tags-column -97)
+ '(org-time-clocksum-use-fractional t)
+ '(org-todo-repeat-to-state "TODO")
+ '(org-use-property-inheritance
+   (quote
+    ("AREA")))
+ '(org-use-speed-commands t)
+ '(org-x-backends
+   (quote
+    (ox-org ox-redmine)))
+ '(org-x-priority-B-silent nil)
+ '(org-x-redmine-title-prefix-function
+   (quote org-x-redmine-title-prefix))
+ '(org-x-redmine-title-prefix-match-function
+   (quote org-x-redmine-title-prefix-match))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
@@ -516,6 +793,49 @@ where I don't expect it to be keeping undo history anyway")
    t)
  '(rst-level-2-face
    ((t
+ '(org-document-title
+   ((((class color)
+      (background light))
+     (:foreground "midnight blue" :weight bold :height 2.0 :family "Helvetica"))
+    (((class color)
+      (background dark))
+     (:foreground "pale turquoise" :weight bold :height 2.0 :family "Helvetica"))
+    (t
+     (:weight bold))))
+ '(org-habit-alert-face
+   ((((background light dark))
+     (:inherit dwa/org-habit :background "#f5f946"))))
+ '(org-habit-alert-future-face
+   ((((background light dark))
+     (:inherit dwa/org-habit :background "#fafca9"))))
+ '(org-habit-clear-face
+   ((((background light dark))
+     (:inherit dwa/org-habit :background "#8270f9"))))
+ '(org-habit-clear-future-face
+   ((((background light dark))
+     (:inherit dwa/org-habit :background "#d6e4fc"))))
+ '(org-habit-overdue-face
+   ((((background light dark))
+     (:inherit dwa/org-habit :background "#f9372d"))))
+ '(org-habit-overdue-future-face
+   ((((background light dark))
+     (:inherit dwa/org-habit :background "#fc9590"))))
+ '(org-habit-ready-face
+   ((((background light dark))
+     (:inherit dwa/org-habit :background "#4df946"))))
+ '(org-habit-ready-future-face
+   ((((background light dark))
+     (:inherit dwa/org-habit :background "#acfca9"))))
+ '(org-scheduled
+   ((((class color)
+      (min-colors 88)
+      (background light))
+     nil)))
+ '(org-upcoming-deadline
+   ((((class color)
+      (min-colors 88)
+      (background light))
+     (:foreground "Brown"))))
      (:inherit nil :background "grey78" :foreground "black")))
    t)
  '(rst-level-3-face
