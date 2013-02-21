f794dc87fbe3983eb537d413c506915c7e62b032
Dave Abrahams <dave@boostpro.com>
2013-02-21 13:59:54 -0800

    Install el-get from master branch if necessary

--- startup/09-el-get.el
+++ startup/09-el-get.el
@@ -1,3 +1,13 @@
 (add-to-list 'load-path (expand-file-name (elhome-path-join user-emacs-directory "el-get" "el-get")))
+
+(unless (require 'el-get nil 'noerror)
+  (with-current-buffer
+      (url-retrieve-synchronously
+       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+    (let (el-get-master-branch)
+      (goto-char (point-max))
+      (eval-print-last-sexp))))
+
 (require 'el-get)
+
 (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/dwamacs/el-get-recipes/personal/")
